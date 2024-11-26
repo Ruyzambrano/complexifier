fc3fe56c619836139934ab69f96438cb88ef2bf9
ruyzambrano <ruyzambrano@gmail.com>
2024-11-26 14:42:57 +0000

    Didn't like the theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'groundwork'
+html_theme = 'classic'
 html_static_path = ['_static']
