fd277f43cc1a1072a79c0868fa851cde049123de
ruyzambrano <ruyzambrano@gmail.com>
2024-11-27 10:03:43 +0000

    Added an extension to see if it works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,8 @@ release = '0.3.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'sphinx.ext.githubpages ']
 
 templates_path = ['_templates']
 exclude_patterns = []
