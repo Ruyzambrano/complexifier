086b9438b1c1117d60b5e28f1e8857414738bfcd
ruyzambrano <ruyzambrano@gmail.com>
2024-11-27 12:23:11 +0000

    Returning to underscores

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,10 @@ release = '0.3.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sphinx_rtd_theme
-
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
-              'sphinx.ext.githubpages']
+              'sphinx.ext.githubpages',
+              'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 html_static_path = ['_static']
