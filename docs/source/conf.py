b19f0c253507bf500234862a54edd7a1403a4cc3
ruyzambrano <ruyzambrano@gmail.com>
2024-11-27 13:07:25 +0000

    Requirements issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
 
+import typo
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
