756131c2e63ab6d59082446439da80bcf306afb5
ruyzambrano <ruyzambrano@gmail.com>
2024-11-27 12:00:55 +0000

    installed the new theme using pip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,11 +28,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-
-
-pygments_style = 'sphinx'
-
 html_logo = "assets/complexifier.jpeg"
